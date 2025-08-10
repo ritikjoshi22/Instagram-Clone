5fecc7f6aac81770f0daeac2cf6b46918661cb43
ritikjoshi22 <joshster.ritik@gmail.com>
2025-08-10 21:48:02 +0545

    Added routes: Routing for posts, profiles, comments, likes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,25 +14,6 @@ class CommentController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request, Post $post)
     {
         $data = $request->validate([
